23867fa009bf0f88d04737a60c77e269edcfbc51
hsuweet <vickiahsu@gmail.com>
2015-11-04 18:43:16 -0800

    cash-register

---
+++ cash-register-starter/js/cash-register.js
@@ -0,0 +1,35 @@
+// Structure
+// ------------------------------------------------
+var form    = document.querySelector("form");
+var input   = document.querySelector("input");
+var entries = document.querySelector(".entries");
+var total   = document.querySelector(".total");
+
+
+// Setup
+// ------------------------------------------------
+var totalValue = 0;
+
+
+// Events
+// ------------------------------------------------
+form.addEventListener("submit", enter);
+
+function enter (event){
+	event.preventDefault();
+
+	//make sure input is number parseFloat
+	var inputValue = parseFloat(input.value);
+
+	//sum
+	totalValue = totalValue + inputValue;
+
+	//make list
+	var newItem = document.createElement("LI");
+
+	newItem.textContent = inputValue;
+
+	entries.appendChild(newItem);
+
+	total.textContent = totalValue;
+}
